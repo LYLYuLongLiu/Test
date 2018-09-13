3871c73a8eed0c0d3920b539d6c40160b174860a
LYL <811688594@qq.com>
2018-09-13 19:26:46 +0800

    Test one

---
+++ struts2_0100/src/main/java/Main.java
@@ -0,0 +1,34 @@
+/**
+ * <p>ClassName: Main</p>
+ * <p>Description:</p>
+ *
+ * @author 刘玉龙
+ * @version 1.0
+ * @date 2018/9/9 11:16
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Digui d = new Digui();
+        System.out.println(d.swit(5));
+    }
+
+
+}
+
+class Digui{
+    public int swit(int x){
+        int sum=0;
+
+        if(x==1) {
+            sum +=10;
+            return sum;
+        }
+        else {
+            sum+=2;
+            sum += swit(x-1);
+        }
+        return sum;
+
+    }
+}
