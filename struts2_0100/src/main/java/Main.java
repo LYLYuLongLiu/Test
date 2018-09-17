cb1850a5e6f36b0cbba4896b1da74087373ef8c0
LYL <811688594@qq.com>
2018-09-17 09:09:42 +0800

    new

--- struts2_0100/src/main/java/Main.java
+++ struts2_0100/src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         Digui d = new Digui();
-        System.out.println(d.swit(3));
+        System.out.println(d.swit(5));
     }
 }
 
---
+++ struts2_0100/src/main/java/New.java
@@ -0,0 +1,13 @@
+/**
+ * <p>ClassName: New</p>
+ * <p>Description:</p>
+ *
+ * @author 刘玉龙
+ * @version 1.0
+ * @date 2018/9/17 9:07
+ */
+public class New {
+
+    public New() {
+    }
+}
